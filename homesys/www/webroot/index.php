3645dd0d142d6332e5ece43ac2402058f2fb8198
gabest11 <gabest11@gmail.com>
2022-03-30 18:07:46 +0200

    index.php redir to github page

---
+++ homesys/www/webroot/index.php
@@ -0,0 +1,17 @@
+<?php
+
+header('Location: https://gabest11.github.io/homesys/');
+exit;
+
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+	<title>Homesys</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Language" content="hu-hu" />
+</head>
+<body>
+	<a href="/download/homesys.php">Homesys legújabb verziójának letöltése</a>
+</body>
